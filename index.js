cdc4743ebe888cc0967feca40ea7e4b68d6a9d5a
Will Hall <will.hall@factorymedia.com>
2017-04-13 09:37:06 +0100

    update program script. Returns letter scores so far

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
+const StationFinder = require('./src/station-finder.es6.js');
+const finder = new StationFinder();
 
+let stationList = finder.fileToArray('./lib/underground-stations.csv');
+stations = finder.stripDuplicateLetters(stationList);
+letterScores = finder.scoreLetters(stations);
+console.log(letterScores);
