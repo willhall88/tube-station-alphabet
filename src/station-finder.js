2db748bcbfe08730fabeeb1dd0d3c7edb66bfd8e
Will Hall <will.hall@factorymedia.com>
2017-04-15 11:44:47 +0100

    Add tests to count letters

--- src/station-finder.js
+++ src/station-finder.js
@@ -4,16 +4,33 @@ const Station = require('./station');
 
 class StationFinder {
   constructor() {
+    this.letters = new Map();
+    this.initLetters();
+  }
+
+  initLetters () {
+    let letterInstances = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => new Letter(letter));
+    letterInstances.forEach(letter => this.letters.set(letter.name, letter));
   }
 
   fileToArray (filename) {
     let file = fs.readFileSync(filename, 'utf8');
     let array = file.split('\r\n');
-    let stations = array.map(value => new Station(value));
-    return stations;
+    this.stations = array.map(value => new Station(value));
+    return this.stations;
   }
 
+  countLetters () {
+    let characters = this.stations.map(station => station.characters).join('');
 
+    this.letters.forEach(letter => {
+      let re = new RegExp(letter.name ,"gi");
+      if (characters.match(re) != null) {
+        let count = characters.match(re).length;
+        letter.setValue(count);
+      }
+    })
+  }
 };
 
 module.exports = StationFinder;
--- test/station-finder.test.js
+++ test/station-finder.test.js
@@ -1,12 +1,25 @@
 const expect = require('chai').expect;
 const StationFinder = require('../src/station-finder.js');
 const finder = new StationFinder();
+const Letter = require('../src/letter');
 
 describe('StationFinder', function() {
+  it('has an array of Letters on initialise', function() {
+    const letters = finder.letters;
+    expect(letters.get('a')).to.be.instanceOf(Letter);
+    expect(letters.size).to.eql(26);
+  });
+
   it('makes an array of stations from file', function() {
     const stations = finder.fileToArray('./lib/test.csv');
     expect(stations).to.be.instanceOf(Array);
     expect(stations.length).to.eql(2);
     expect(stations[0].name).to.eql('Acton Town');
+  });
+
+  it('counts the number of occurrences for each letter', function() {
+    const stations = finder.fileToArray('./lib/test.csv');
+    finder.countLetters()
+    expect(finder.letters.get('a').value).to.eql(2);
   });
 });
