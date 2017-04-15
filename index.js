f222b397accb397458dbce3f66278a191c53f72e
Will Hall <will.hall@factorymedia.com>
2017-04-15 16:47:02 +0100

    Add npm script

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
-const StationFinder = require('./src/station-finder.es6.js');
+const StationFinder = require('./src/station-finder');
 const finder = new StationFinder();
 
 let stationList = finder.fileToArray('./lib/underground-stations.csv');
-stations = finder.stripDuplicateLetters(stationList);
-letterScores = finder.scoreLetters(stations);
-console.log(letterScores);
+
+
+console.log(`
+  There are ${finder.lettersToBeFound()} letters left to find`);
+
+while (finder.lettersToBeFound() > 0) {
+  finder.countLetters();
+  finder.scoreStations();
+  console.log(`
+    Selecting ${finder.valuableStation().name.toUpperCase()}
+    and removing ${finder.valuableStation().characters.length} letters`);
+  finder.findAndUpdate();
+  console.log(`    There are ${finder.lettersToBeFound()} letters left to find`);
+}
+
+console.log(``);
+console.log(`All letters have been found`);
+console.log(`The tube stations are:`)
+finder.selectedStations().forEach(station =>{
+  console.log(station.name.toUpperCase());
+})
+
